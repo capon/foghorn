23fb5d7897026552b46ee9e54bfb0d2321bcff8e
claudiob <claudiob@gmail.com>
2014-10-21 13:06:46 -0700

    Only leave one controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  def index
+    `curl -s -X POST --data-urlencode 'payload={"text": "Hello! I am Foghorn! I lost my memory!"}' #{ENV['SLACK_WEBHOOK_URL']}`
+    render nothing: true
+  end
 end
