9251cd1448aadf443525f385614c361cb0153530
claudiob <claudiob@gmail.com>
2015-09-10 23:33:04 -0700

    Turn into DATING bot

--- app/models/response/botlibre.rb
+++ app/models/response/botlibre.rb
@@ -5,7 +5,7 @@ module Response
   class Botlibre < Base
     def to_s
       request.text.slice! request.trigger_word
-      params = "instance=165&message=#{CGI::escape request.text.lstrip}"
+      params = "instance=145&message=#{CGI::escape request.text.lstrip}"
       url = "http://www.botlibre.com/rest/botlibre/form-chat?#{params}"
       body = open(url).read
       body[/<message>(.*?)<\/message>/m, 1]
