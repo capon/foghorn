f760466278b929761c9b368d0b7905de92a9006d
claudiob <claudiob@gmail.com>
2015-10-05 16:25:11 -0700

    Debug params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def index
+    p request_params
     request = Request.new request_params
     request.reply if request.valid?
     render nothing: true
